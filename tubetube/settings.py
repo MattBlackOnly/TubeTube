3fb7b9e3c614411afff0f3d1e503ddf56d6cb0da
Matt Black <152306888+MattBlackOnly@users.noreply.github.com>
2024-09-05 13:39:56 +0100

    Fix Settings File Location

--- tubetube/settings.py
+++ tubetube/settings.py
@@ -20,7 +20,9 @@ class Config:
 
 class Settings:
     def __init__(self):
-        self.file_path = "settings.yaml"
+        self.config_folder = "/config"
+        self.settings_file_path = os.path.join(self.config_folder, "settings.yaml")
+
         self.folder_locations = self._load_settings()
         self.audio_locations, self.video_locations = self._categorise_locations()
 
@@ -29,19 +31,19 @@ class Settings:
         for folder_name in all_folders:
             os.makedirs(os.path.join("/data", folder_name), exist_ok=True)
 
-        os.makedirs("config", exist_ok=True)
+        os.makedirs(self.config_folder, exist_ok=True)
 
-        cookies_json_file = os.path.join("config", "cookies.txt")
+        cookies_json_file = os.path.join(self.config_folder, "cookies.txt")
         self.cookies_file = cookies_json_file if os.path.exists(cookies_json_file) else None
 
     def _load_settings(self):
-        if not os.path.exists(self.file_path):
-            with open(self.file_path, "w") as file:
+        if not os.path.exists(self.settings_file_path):
+            with open(self.settings_file_path, "w") as file:
                 yaml.safe_dump(Config.DEFAULT_FOLDER_LOCATIONS, file, default_flow_style=False)
             return Config.DEFAULT_FOLDER_LOCATIONS
 
         try:
-            with open(self.file_path, "r") as file:
+            with open(self.settings_file_path, "r") as file:
                 return yaml.safe_load(file) or Config.DEFAULT_FOLDER_LOCATIONS
 
         except yaml.YAMLError as e:
