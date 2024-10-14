2b1680d77048d4578c2764c9499d08d4074860e5
Matt Black <152306888+MattBlackOnly@users.noreply.github.com>
2024-10-14 12:53:39 +0100

    Update yt_downloader.py

--- tubetube/yt_downloader.py
+++ tubetube/yt_downloader.py
@@ -195,7 +195,6 @@ class DownloadManager:
 
         post_processors.append({"key": "FFmpegThumbnailsConvertor", "format": "png", "when": "before_dl"})
         post_processors.append({"key": "EmbedThumbnail"})
-        post_processors.append({"key": "FFmpegMetadata"})
 
         if not item.get("audio_only"):
             ydl_opts["merge_output_format"] = "mp4"
