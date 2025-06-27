78fd387ba7eec049671680c84b889ff0483b7b49
Matt Black <152306888+MattBlackOnly@users.noreply.github.com>
2025-06-27 17:17:31 +0100

    Update start_config.py
    [skip ci]

--- start_config.py
+++ start_config.py
@@ -1,5 +1,6 @@
 bind = "0.0.0.0:6543"
 workers = 1
-threads = 1
+threads = 4
 timeout = 180
-worker_class = "geventwebsocket.gunicorn.workers.GeventWebSocketWorker"
+preload_app = False
+worker_class = "gthread"
